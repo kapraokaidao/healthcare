fc1a4f70f763c03c138ba2daa7f7b31d7f1d2eea
Kao Oat <tanawit139@gmail.com>
2020-11-09 15:08:05 +0700

    refactor: use async/await

--- backend/healthcare/src/stellar/stellar.controller.ts
+++ backend/healthcare/src/stellar/stellar.controller.ts
@@ -2,30 +2,32 @@ import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { StellarService } from './stellar.service';
 import { PublicAPI } from '../decorators/public-api.decorator';
 import { ApiTags } from '@nestjs/swagger';
-import { IssueTokenDto } from './stellar.dto';
+import { CreateAccountResponse, IssueTokenDto } from './stellar.dto';
+import { Horizon } from "stellar-sdk";
+import BalanceLine = Horizon.BalanceLine;
 
-@PublicAPI()
+
 @ApiTags('Stellar')
 @Controller('stellar')
 export class StellarController {
   constructor(private readonly stellarService: StellarService) {}
 
-  @Get('account')
-  async createAccount() {
+  @Post('account')
+  async createAccount(): Promise<CreateAccountResponse> {
     return await this.stellarService.createAccount();
   }
 
   @Get('balance/:secret')
   async getBalanceBySecret(
     @Param('secret') secret: string
-  ): Promise<string> {
+  ): Promise<BalanceLine[]> {
     return await this.stellarService.getBalanceBySecret(secret);
   }
 
   @Post('service')
   async issueToken(
     @Body() dto: IssueTokenDto
-  ): Promise<string>{
+  ): Promise<void>{
     return this.stellarService.issueToken(dto.issueingSecret, dto.receivingSecret, dto.serviceName, dto.amount);
   }
 }
