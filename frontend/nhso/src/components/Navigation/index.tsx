c332701c245172fea3b6bd2fd3d6f7b050f846b1
N <N@N.N>
2020-12-10 21:22:47 +0700

    material-ui

--- frontend/nhso/src/components/Navigation/index.tsx
+++ frontend/nhso/src/components/Navigation/index.tsx
@@ -1,56 +1,185 @@
-import React, { useCallback, useContext, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { AuthStoreContext } from '../../stores';
 import { observer } from 'mobx-react-lite';
-import './style.scss';
-import home from './../../images/home-icon.png';
-import manageToken from './../../images/manage-token.png';
-import manageAccount from './../../images/manage-account.png';
-import kyc from './../../images/KYC.png';
-import account from './../../images/account.png';
+import clsx from 'clsx';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
 import Button from '@material-ui/core/Button';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import AccountBalanceWalletIcon from '@material-ui/icons/AccountBalanceWallet';
+import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount';
+import CameraAltIcon from '@material-ui/icons/CameraAlt';
+import { useLocation } from 'react-router-dom';
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+	root: {
+		flexGrow: 1,
+	},
+	appBar: {
+		transition: theme.transitions.create(['margin', 'width'], {
+			easing: theme.transitions.easing.sharp,
+			duration: theme.transitions.duration.leavingScreen,
+		}),
+	},
+	appBarShift: {
+		width: `calc(100% - ${drawerWidth}px)`,
+		marginLeft: drawerWidth,
+		transition: theme.transitions.create(['margin', 'width'], {
+			easing: theme.transitions.easing.easeOut,
+			duration: theme.transitions.duration.enteringScreen,
+		}),
+	},
+	menuButton: {
+		marginRight: theme.spacing(2),
+	},
+	title: {
+		flexGrow: 1,
+	},
+	drawer: {
+		width: drawerWidth,
+		flexShrink: 0,
+	},
+	drawerPaper: {
+		width: drawerWidth,
+	},
+	drawerHeader: {
+		display: 'flex',
+		alignItems: 'center',
+		padding: theme.spacing(0, 1),
+		...theme.mixins.toolbar,
+		justifyContent: 'flex-end',
+	},
+	content: {
+		flexGrow: 1,
+		padding: theme.spacing(3),
+		transition: theme.transitions.create('margin', {
+			easing: theme.transitions.easing.sharp,
+			duration: theme.transitions.duration.leavingScreen,
+		}),
+		marginLeft: -drawerWidth,
+	},
+	contentShift: {
+		transition: theme.transitions.create('margin', {
+			easing: theme.transitions.easing.easeOut,
+			duration: theme.transitions.duration.enteringScreen,
+		}),
+		marginLeft: 0,
+	},
+	hide: {
+		display: 'none',
+	},
+}));
 
 const Navigation = observer(() => {
-	const authStore = useContext(AuthStoreContext);
-	const [showMenu, setShowMenu] = useState(false);
-	const handleButton = useCallback(() => {
-		setShowMenu(!showMenu);
-	}, [showMenu]);
+	const location = useLocation();
+	const [show, setShow] = useState(true);
+	useEffect(() => {
+		if (location.pathname === '/signin') {
+			setShow(false);
+		}
+	}, []);
 
+	const authStore = useContext(AuthStoreContext);
+	const classes = useStyles();
+	const theme = useTheme();
+	const [open, setOpen] = React.useState(false);
+
+	const handleDrawerOpen = () => {
+		setOpen(true);
+	};
+
+	const handleDrawerClose = () => {
+		setOpen(false);
+	};
 	return (
 		<>
-			<div className="navigation px-4">
-				<div>
-					<button onClick={handleButton}>☰</button>
-					<span className="ml-8">Home</span>
-				</div>
-				<div className="ml-auto">
-					<div className="flex items-center">
-						<span className="mr-3">xx {authStore.user.firstname}</span>
-						<img src={account} className="h-full mr-3" />
-						<Button variant="contained" color="secondary">
-							Sign Out
-						</Button>
-					</div>
-				</div>
-			</div>
-			{showMenu && (
-				<div className="menu shadow-lg">
-					<div>
-						<img src={home} />
-						<span>Home</span>
-					</div>
-					<div>
-						<img src={manageToken} />
-						<span>Manage Token</span>
-					</div>
-					<div>
-						<img src={manageAccount} />
-						<span>Manage Account</span>
-					</div>
-					<div>
-						<img src={kyc} />
-						<span>KYC</span>
-					</div>
+			{show && (
+				<div className={classes.root}>
+					<AppBar
+						position="fixed"
+						className={clsx(classes.appBar, {
+							[classes.appBarShift]: open,
+						})}
+					>
+						<Toolbar>
+							<IconButton
+								color="inherit"
+								aria-label="open drawer"
+								onClick={handleDrawerOpen}
+								edge="start"
+								className={clsx(classes.menuButton, open && classes.hide)}
+							>
+								<MenuIcon />
+							</IconButton>
+							<Typography variant="h6" className={classes.title}>
+								Home
+							</Typography>
+							<div className="center">
+								<span>Username</span>
+								<AccountCircle fontSize="large" className="ml-10 mr-10" />
+								<Button variant="contained" color="secondary">
+									Sign Out
+								</Button>
+							</div>
+						</Toolbar>
+					</AppBar>
+					<Drawer
+						className={classes.drawer}
+						variant="persistent"
+						anchor="left"
+						open={open}
+						classes={{
+							paper: classes.drawerPaper,
+						}}
+					>
+						<div className={classes.drawerHeader}>
+							<IconButton onClick={handleDrawerClose}>
+								{theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+							</IconButton>
+						</div>
+						<Divider />
+						<List>
+							<ListItem button>
+								<ListItemIcon>
+									<AccountCircleIcon fontSize="large" />
+								</ListItemIcon>
+								<ListItemText primary="Home" />
+							</ListItem>
+							<ListItem button>
+								<ListItemIcon>
+									<AccountBalanceWalletIcon fontSize="large" />
+								</ListItemIcon>
+								<ListItemText primary="Manage Token" />
+							</ListItem>
+							<ListItem button>
+								<ListItemIcon>
+									<SupervisorAccountIcon fontSize="large" />
+								</ListItemIcon>
+								<ListItemText primary="Manage Account" />
+							</ListItem>
+							<ListItem button>
+								<ListItemIcon>
+									<CameraAltIcon fontSize="large" />
+								</ListItemIcon>
+								<ListItemText primary="KYC" />
+							</ListItem>
+						</List>
+					</Drawer>
 				</div>
 			)}
 		</>
--- frontend/nhso/src/pages/Signin/index.tsx
+++ frontend/nhso/src/pages/Signin/index.tsx
@@ -1,13 +1,12 @@
 import React, { useContext, useState } from 'react';
 import { AuthStoreContext } from '../../stores';
 import { observer } from 'mobx-react-lite';
-import { Formik, Field, Form, ErrorMessage } from 'formik';
-import * as Yup from 'yup';
 import { SigninType } from '../../stores/AuthStore';
 import { Link, useHistory } from 'react-router-dom';
 import nhso from '../../images/nhso.png';
 import Button from '@material-ui/core/Button';
 import './style.scss';
+import TextField from '@material-ui/core/TextField';
 
 const initialValue: SigninType = {
 	username: '',
@@ -17,36 +16,33 @@ const initialValue: SigninType = {
 const Signin = observer(() => {
 	const authStore = useContext(AuthStoreContext);
 	const [history] = useState(useHistory());
-	const [errors, setErrors] = useState('');
 
 	return (
 		<>
-			<img src={nhso} className="signin-image" />
-			<Formik
-				initialValues={initialValue}
-				validationSchema={Yup.object({
-					username: Yup.string().required('Required'),
-					password: Yup.string().min(8, 'Must be more than 8 characters').required('Required'),
-				})}
-				onSubmit={async (values, { setSubmitting }) => {
-					const response = await authStore.signin(values);
-					setErrors(response);
-					setSubmitting(false);
-					if (response === true) history.push('/');
-				}}
-			>
-				<Form className="signin-form">
-					<Field name="username" type="text" placeholder="Username" className="mt-10" />
-					<ErrorMessage name="username" />
-
-					<Field name="password" type="password" placeholder="Password" className="mt-4" />
-					<ErrorMessage name="password" />
-
-					<Button variant="contained" color="primary" type="submit">
-						Login
-					</Button>
-				</Form>
-			</Formik>
+			<div className="signin">
+				<div>
+					<div className="center">
+						<img src={nhso} />
+					</div>
+					<div className="mt-15">
+						<TextField id="outlined-username-input" label="Username" variant="outlined" fullWidth />
+					</div>
+					<div className="mt-15">
+						<TextField
+							id="outlined-password-input"
+							label="Password"
+							type="password"
+							variant="outlined"
+							fullWidth
+						/>
+					</div>
+					<div className="center mt-15">
+						<Button variant="contained" color="primary" size="large">
+							Login
+						</Button>
+					</div>
+				</div>
+			</div>
 		</>
 	);
 });
