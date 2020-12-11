6a7754ad19523a47fdd52b70e3838fc57b6ced91
Thanadol <thanafr1998@hotmail.com>
2020-12-11 16:42:51 +0700

    (fix) set for using api

--- frontend/nhso/src/pages/SiteHome/index.tsx
+++ frontend/nhso/src/pages/SiteHome/index.tsx
@@ -4,46 +4,52 @@ import nhso from '../../images/nhso.png';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 
-//Mock up data of user
-const user = { username:'nhso', firstname:'Thanadol', surname:'Rungjitwaranon', address:'home', phone:'08x-xxx-xxxx', role:'admin'}
-
 const SiteHome = observer(() =>  {
+	const [user, setUser] = React.useState({
+		username:'nhso',
+		firstname:'Thanadol',
+		surname:'Rungjitwaranon',
+		address:'home',
+		phone:'08x-xxx-xxxx',
+		role:'admin'
+    });
+	
 	return (
 		<>
-		<div className="sitehome">
-			<div>
-				<div className="center">
-					<img src={nhso} />
-				</div>
-				<div className="mt-15">
-					<Typography variant="h2" gutterBottom align="center">
-						Username : {user.username}
-					</Typography>
-				</div>
+			<div className="sitehome">
 				<div>
-					<Box border={1} width={800} height={350}>
+					<div className="center">
+						<img src={nhso} />
+					</div>
+					<div className="mt-15">
 						<Typography variant="h2" gutterBottom align="center">
-							Info
+							Username : {user.username}
 						</Typography>
-						<Typography variant="h5" gutterBottom align="left">
-							Firstname : {user.firstname}
-						</Typography>
-						<Typography variant="h5" gutterBottom align="left">
-							Surname : {user.surname}
-						</Typography>
-						<Typography variant="h5" gutterBottom align="left">
-							Address : {user.address}
-						</Typography>
-						<Typography variant="h5" gutterBottom align="left">
-							Phone : {user.phone}
-						</Typography>
-						<Typography variant="h5" gutterBottom align="left">
-							Role : {user.role}
-						</Typography>
-					</Box>
+					</div>
+					<div className="mt-15">
+						<Box border={1} width={800} height={350}>
+							<Typography variant="h2" gutterBottom align="center">
+								Info
+							</Typography>
+							<Typography variant="h5" gutterBottom align="left">
+								Firstname : {user.firstname}
+							</Typography>
+							<Typography variant="h5" gutterBottom align="left">
+								Surname : {user.surname}
+							</Typography>
+							<Typography variant="h5" gutterBottom align="left">
+								Address : {user.address}
+							</Typography>
+							<Typography variant="h5" gutterBottom align="left">
+								Phone : {user.phone}
+							</Typography>
+							<Typography variant="h5" gutterBottom align="left">
+								Role : {user.role}
+							</Typography>
+						</Box>
+					</div>
 				</div>
 			</div>
-		</div>
 		</>
 	);
 });
