afb18c838180127b40f68d5f8ee6792c7b8f8a9a
N <N@N.N>
2020-12-15 23:30:04 +0700

    kyc

---
+++ frontend/nhso/src/pages/KYC/index.tsx
@@ -0,0 +1,97 @@
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import AddIcon from '@material-ui/icons/Add';
+import IconButton from '@material-ui/core/IconButton';
+import { DataGrid, ColDef, ValueFormatterParams } from '@material-ui/data-grid';
+import { FilterUser, Role, Gender, User } from '../../types';
+import axios from 'axios';
+import './style.scss';
+import { useHistory } from 'react-router-dom';
+
+type Row = {
+	id: number;
+	nationalId: string;
+	firstname: string;
+	surname: string;
+	gender: Gender;
+};
+
+const KYC = () => {
+	const [history] = useState(useHistory());
+	const [users, setUsers] = useState<User[]>([]);
+	useEffect(() => {
+		axios
+			.get('/user/kyc', {
+				params: {
+					page: 0,
+					pageSize: 0,
+					approved: false,
+					ready: true,
+				},
+			})
+			.then(({ data: { data } }) => {
+				setUsers(data);
+			});
+	}, []);
+
+	const rows = useMemo(() => {
+		const data: Row[] = [];
+		for (const user of users) {
+			if (user.patient) {
+				data.push({
+					id: user.id,
+					nationalId: user.patient.nationalId,
+					firstname: user.firstname,
+					surname: user.surname,
+					gender: user.patient.gender,
+				});
+			}
+		}
+		return data;
+	}, [users]);
+
+	const columns: ColDef[] = useMemo(() => {
+		return [
+			{ field: 'id', hide: true },
+			{ field: 'nationalId', headerName: 'National ID', width: 150 },
+			{ field: 'firstname', headerName: 'First Name', width: 120 },
+			{ field: 'surname', headerName: 'Surname', width: 120 },
+			{ field: 'gender', headerName: 'Gender', flex: 0.2 },
+			{
+				field: '',
+				headerName: '',
+				width: 100,
+				disableClickEventBubbling: true,
+				renderCell: (params: ValueFormatterParams) => {
+					const onClick = () => {
+						const row = params.row as Row;
+						history.push(`/kyc/${row.id}`);
+					};
+					return (
+						<strong>
+							<Button onClick={onClick} variant="contained" color="primary" size="small">
+								View
+							</Button>
+						</strong>
+					);
+				},
+			},
+		];
+	}, []);
+
+	return (
+		<>
+			<Grid container spacing={3}>
+				<h1>KYC Pending List</h1>
+			</Grid>
+
+			<div style={{ height: 700, width: '100%' }}>
+				<DataGrid rows={rows} columns={columns} pageSize={20} rowsPerPageOptions={[5, 10, 20]} pagination />
+			</div>
+		</>
+	);
+};
+
+export default KYC;
--- frontend/nhso/src/pages/KYCConfirm/index.tsx
+++ frontend/nhso/src/pages/KYCConfirm/index.tsx
@@ -1,137 +1,162 @@
-import React from 'react';
-import { observer } from 'mobx-react-lite';
+import React, { useCallback, useEffect, useState } from 'react';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
-import taan1 from '../../images/taan1.png';
-import taan2 from '../../images/taan2.png';
+import axios from 'axios';
 import './style.scss';
+import { User } from '../../types';
+import { useHistory } from 'react-router-dom';
 
-const KYCConfirm = observer(() =>  {
-	const [user, setUser] = React.useState({
-		firstname:'Thanawat',
-        surname:'Jierawatanakanok',
-        gender:'Male',
-        birthdate:'18-08-1998',
-        idnumber:'1-1xxx-xxxxx-xx-x',
-		address:'Home',
-		phone:'08x-xxx-xxxx'
-    });
-	
+type Props = {
+	match: {
+		params: {
+			id: string;
+		};
+	};
+};
+
+const KYCConfirm = (props: Props) => {
+	const [userId] = useState(props.match.params.id);
+	const [user, setUser] = useState<User>();
+	const [history] = useState(useHistory());
+
+	useEffect(() => {
+		axios.get(`/user/${userId}`).then(({ data }) => {
+			setUser(data);
+		});
+	}, [userId]);
+
+	const back = useCallback(() => {
+		history.push('/kyc');
+	}, [history]);
+
+	const approve = useCallback(() => {
+		console.log('approve', userId);
+	}, [userId]);
+
+	const reject = useCallback(() => {
+		console.log('reject', userId);
+	}, [userId]);
+
 	return (
 		<>
 			<div className="sitehome">
-                <div className="mt-15">
-                    <Button variant="contained" color="default" size="large">
-                        {'<'} Back
-                    </Button>
-                </div>
-                <div className="mt-15">
-                    <Typography variant="h3" gutterBottom align="left">
-                        {user.firstname}  {user.surname}
+				<div className="mt-15">
+					<Button variant="contained" color="default" size="large" onClick={back}>
+						{'<'} Back
+					</Button>
+				</div>
+				<div className="mt-15">
+					<Typography variant="h3" gutterBottom align="left">
+						{user?.firstname} {user?.surname}
 					</Typography>
-                </div>
-                <Grid container spacing={2}>
-                    <Grid item xs={4} container alignItems="flex-end">
-                        <Typography variant="h5" gutterBottom align="left">
-                            Firstname
-                        </Typography>
-                    </Grid>
-                    <Grid item xs={8}>
-                        <Typography variant="h6" gutterBottom align="left">
-                            {user.firstname}
-                        </Typography>
-                    </Grid>
-                    <Grid item xs={4} container alignItems="flex-end">
-                        <Typography variant="h5" gutterBottom align="left">
-                            Surname
-                        </Typography>
-                    </Grid>
-                    <Grid item xs={8}>
-                        <Typography variant="h6" gutterBottom align="left">
-                            {user.surname}
-                        </Typography>
-                    </Grid>
-                    <Grid item xs={4} container alignItems="flex-end">
-                        <Typography variant="h5" gutterBottom align="left">
-                            Gender
-                        </Typography>
-                    </Grid>
-                    <Grid item xs={8}>
-                        <Typography variant="h6" gutterBottom align="left">
-                            {user.gender}
-                        </Typography>
-                    </Grid>
-                    <Grid item xs={4} container alignItems="flex-end">
-                        <Typography variant="h5" gutterBottom align="left">
-                            Birthdate
-                        </Typography>
-                    </Grid>
-                    <Grid item xs={8}>
-                        <Typography variant="h6" gutterBottom align="left">
-                            {user.birthdate}
-                        </Typography>
-                    </Grid>
-                    <Grid item xs={4} container alignItems="flex-end">
-                        <Typography variant="h5" gutterBottom align="left">
-                            ID Number
-                        </Typography>
-                    </Grid>
-                    <Grid item xs={8}>
-                        <Typography variant="h6" gutterBottom align="left">
-                            {user.idnumber}
-                        </Typography>
-                    </Grid>
-                    <Grid item xs={4} container alignItems="flex-end">
-                        <Typography variant="h5" gutterBottom align="left">
-                            Address
-                        </Typography>
-                    </Grid>
-                    <Grid item xs={8}>
-                        <Typography variant="h6" gutterBottom align="left">
-                            {user.address}
-                        </Typography>
-                    </Grid>
-                    <Grid item xs={4} container alignItems="flex-end">
-                        <Typography variant="h5" gutterBottom align="left">
-                            Phone Number
-                        </Typography>
-                    </Grid>
-                    <Grid item xs={8}>
-                        <Typography variant="h6" gutterBottom align="left">
-                            {user.phone}
-                        </Typography>
-                    </Grid>
-                    <Grid item xs={12} container alignItems="flex-end">
-                        <Typography variant="h5" gutterBottom align="left">
-                            Image
-                        </Typography>
-                    </Grid>
-                </Grid>
-                <div className="center">
-                    <img className="photo" src={taan1} />
-                </div>
-                <div className="center">
-                    <img className="photo" src={taan2} />
-                </div>
-                <div className="mt-15">
-                    <Grid container spacing={0}>
-                        <Grid item xs={8}></Grid>
-                        <Grid item xs={2}>
-                            <Button variant="contained" color="secondary" size="large">
-                                Delete
-                            </Button>
-                        </Grid>
-                        <Grid item xs={2}>
-                            <Button variant="contained" color="primary" size="large">
-                                Confirm
-                            </Button>
-                        </Grid>
-                    </Grid>
-                </div>
+				</div>
+				<Grid container spacing={2}>
+					<Grid item xs={4} container alignItems="flex-end">
+						<Typography variant="h5" gutterBottom align="left">
+							Firstname
+						</Typography>
+					</Grid>
+					<Grid item xs={8}>
+						<Typography variant="h6" gutterBottom align="left">
+							{user?.firstname}
+						</Typography>
+					</Grid>
+					<Grid item xs={4} container alignItems="flex-end">
+						<Typography variant="h5" gutterBottom align="left">
+							Surname
+						</Typography>
+					</Grid>
+					<Grid item xs={8}>
+						<Typography variant="h6" gutterBottom align="left">
+							{user?.surname}
+						</Typography>
+					</Grid>
+					<Grid item xs={4} container alignItems="flex-end">
+						<Typography variant="h5" gutterBottom align="left">
+							Gender
+						</Typography>
+					</Grid>
+					<Grid item xs={8}>
+						<Typography variant="h6" gutterBottom align="left">
+							{user?.patient?.gender}
+						</Typography>
+					</Grid>
+					<Grid item xs={4} container alignItems="flex-end">
+						<Typography variant="h5" gutterBottom align="left">
+							Birthdate
+						</Typography>
+					</Grid>
+					<Grid item xs={8}>
+						<Typography variant="h6" gutterBottom align="left">
+							{user?.patient?.birthDate}
+						</Typography>
+					</Grid>
+					<Grid item xs={4} container alignItems="flex-end">
+						<Typography variant="h5" gutterBottom align="left">
+							National Id
+						</Typography>
+					</Grid>
+					<Grid item xs={8}>
+						<Typography variant="h6" gutterBottom align="left">
+							{user?.patient?.nationalId}
+						</Typography>
+					</Grid>
+					<Grid item xs={4} container alignItems="flex-end">
+						<Typography variant="h5" gutterBottom align="left">
+							Address
+						</Typography>
+					</Grid>
+					<Grid item xs={8}>
+						<Typography variant="h6" gutterBottom align="left">
+							{user?.address}
+						</Typography>
+					</Grid>
+					<Grid item xs={4} container alignItems="flex-end">
+						<Typography variant="h5" gutterBottom align="left">
+							Phone Number
+						</Typography>
+					</Grid>
+					<Grid item xs={8}>
+						<Typography variant="h6" gutterBottom align="left">
+							{user?.phone}
+						</Typography>
+					</Grid>
+					<Grid item xs={4} container alignItems="flex-end">
+						<Typography variant="h5" gutterBottom align="left">
+							National Id Image
+						</Typography>
+					</Grid>
+					<Grid item xs={8}>
+						<img className="photo" src={user?.patient?.nationalIdImage} />
+					</Grid>
+					<Grid item xs={4} container alignItems="flex-end">
+						<Typography variant="h5" gutterBottom align="left">
+							Selfie Image
+						</Typography>
+					</Grid>
+					<Grid item xs={8}>
+						<img className="photo" src={user?.patient?.selfieImage} />
+					</Grid>
+				</Grid>
+				<div className="mt-15">
+					<Grid container spacing={0}>
+						<Grid item xs={8}></Grid>
+						<Grid item xs={2}>
+							<Button variant="contained" color="secondary" size="large" onClick={reject}>
+								Reject
+							</Button>
+						</Grid>
+						<Grid item xs={2}>
+							<Button variant="contained" color="primary" size="large" onClick={approve}>
+								Approve
+							</Button>
+						</Grid>
+					</Grid>
+				</div>
 			</div>
 		</>
 	);
-});
+};
 
 export default KYCConfirm;
