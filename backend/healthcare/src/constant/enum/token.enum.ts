da31cd709fab0df5dbd22b8588212634c878b4ba
Thanawat <taan02991@hotmail.com>
2020-12-16 02:56:32 +0700

    feat(token): update service entity

---
+++ backend/healthcare/src/constant/enum/token.enum.ts
@@ -0,0 +1,4 @@
+export enum TokenType {
+    Special = "Special",
+    General = "General",
+  }
--- backend/healthcare/src/entities/healthcare-token.entity.ts
+++ backend/healthcare/src/entities/healthcare-token.entity.ts
@@ -7,6 +7,8 @@ import {
   UpdateDateColumn,
 } from "typeorm";
 import { ApiProperty } from "@nestjs/swagger";
+import { UserGender } from "../constant/enum/user.enum";
+import { TokenType } from "../constant/enum/token.enum";
 
 @Entity()
 export class HealthcareToken {
@@ -18,24 +20,56 @@ export class HealthcareToken {
   name: string;
 
   @ApiProperty()
-  @Column()
-  assetName: string;
+  @Column({ name: "token_type" })
+  token_type: TokenType;
 
   @ApiProperty()
-  @Column("boolean")
+  @Column({ nullable: true })
+  description: string;
+
+  @ApiProperty()
+  @Column("int", { name: "total_token" })
+  totalToken: number;
+
+  @ApiProperty()
+  @Column("int", { name: "remaining_token" })
+  remainingToken: number;
+
+  @ApiProperty()
+  @Column("boolean", { name: "is_active" , default: true })
   isActive: boolean;
 
   @ApiProperty()
-  @Column("int")
-  quantity: number;
-
-  @ApiProperty()
-  @Column()
+  @Column({ name: "issuing_public_key" })
   issuingPublicKey: string;
 
   @ApiProperty()
-  @Column()
+  @Column({ name: "receiving_public_key" })
   receivingPublicKey: string;
+
+  @ApiProperty()
+  @Column({ name: "start_time", nullable: true, default: null })
+  startTime: Date;
+
+  @ApiProperty()
+  @Column({ name: "end_time", nullable: true, default: null })
+  endTime: Date;
+
+  @ApiProperty()
+  @Column({ name: "start_birthdate", nullable: true, default: null })
+  startBirthdate: Date;
+
+  @ApiProperty()
+  @Column({ name: "end_birthdate", nullable: true, default: null })
+  endBirthdate: Date;
+
+  @ApiProperty()
+  @Column({ nullable: true, default: null })
+  gender: UserGender;
+
+  @ApiProperty()
+  @Column("int", { name: "token_per_person" })
+  tokenPerPerson: number;
 
   @CreateDateColumn({ update: false, name: "created_date" })
   createdDate: Date;
