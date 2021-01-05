ff3a3ee7c4e42bedd9568dc13d45f343a4e5dc86
Kao Oat <tanawit139@gmail.com>
2021-01-05 18:10:44 +0700

    sentry: remove event tracing

--- backend/healthcare/src/main.ts
+++ backend/healthcare/src/main.ts
@@ -1,31 +1,20 @@
 import { NestFactory, Reflector } from "@nestjs/core";
 import { ConfigService } from "@nestjs/config";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
-import { ExpressAdapter } from "@nestjs/platform-express";
-import * as express from "express";
 import * as helmet from "helmet";
 import * as Sentry from "@sentry/node";
-import * as Tracing from "@sentry/tracing";
 import { AppModule } from "./app.module";
 import { JwtAuthGuard } from "./guards/jwt-auth.guard";
 import { SentryInterceptor } from "./interceptors/sentry.interceptor";
 
 async function bootstrap() {
-  const expressApp = express();
-  const app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
+  const app = await NestFactory.create(AppModule);
   const configService: ConfigService = app.get(ConfigService);
 
   Sentry.init({
     dsn: configService.get<string>("sentry.dsn"),
     enabled: configService.get<boolean>("sentry.enable"),
-    integrations: [
-      new Sentry.Integrations.Http({ tracing: true }),
-      new Tracing.Integrations.Express({ app: expressApp }),
-    ],
-    tracesSampleRate: 1.0,
   });
-  app.use(Sentry.Handlers.requestHandler() as express.RequestHandler);
-  app.use(Sentry.Handlers.tracingHandler());
   app.useGlobalInterceptors(new SentryInterceptor());
 
   app.use(helmet());
