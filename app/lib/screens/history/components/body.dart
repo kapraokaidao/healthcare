e092f18a635b6cb05cd7d0889458ba489033f64e
Thanadol <thanafr1998@hotmail.com>
2021-01-27 18:31:54 +0700

    TODO

--- app/lib/screens/history/components/body.dart
+++ app/lib/screens/history/components/body.dart
@@ -7,8 +7,10 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class Body extends StatelessWidget {
   Future<dynamic> fetchUser() async {
-    final response = await HttpClient.get(path: '/healthcare-token/balance');
-    //print(response);
+    print('hello123123123123');
+    final response = await HttpClient.get(path: "/transaction");
+    print('done213123123213123123');
+    print(response);
     return response;
   }
 
@@ -26,23 +28,26 @@ class Body extends StatelessWidget {
             future: fetchUser(),
             builder: (BuildContext context, AsyncSnapshot snapshot) {
               if (snapshot.hasData) {
-                final users = snapshot.data["data"];
+                print(snapshot);
+                final users = snapshot.data;
+                print(users);
                 List<Map<String, dynamic>> show = new List();
                 for (var data in users) {
                   show.add({
                     "serviceId": data['healthcareToken']['id'],
                     "name": data['healthcareToken']['name'],
-                    "balance": data['balance'].toString(),
+                    "hospital": data['destinationUser']['hospital']['fullname'],
+                    "date": data['destinationUser']['updatedDate'],
                   });
                 }
+                //print(show);
                 return Container(
                     alignment: Alignment.topCenter,
-                    margin: EdgeInsets.symmetric(vertical: 70, horizontal: 0),
                     child: ConstrainedBox(
                         constraints: BoxConstraints(minWidth: 500),
                         child: DataTable(
                           headingRowColor: MaterialStateColor.resolveWith(
-                              (states) => Colors.orange),
+                              (states) => Colors.red),
                           columns: const <DataColumn>[
                             DataColumn(
                               label: Text(
@@ -55,7 +60,7 @@ class Body extends StatelessWidget {
                             ),
                             DataColumn(
                               label: Text(
-                                'Quality',
+                                'Date',
                                 style: TextStyle(
                                     fontSize: 20,
                                     fontStyle: FontStyle.italic,
@@ -75,7 +80,7 @@ class Body extends StatelessWidget {
                                   _viewTokenDetail(context, user["serviceId"]);
                                 }),
                                 DataCell(
-                                    Text(user["balance"],
+                                    Text(user["date"],
                                         textAlign: TextAlign.right,
                                         style: TextStyle(
                                           fontSize: 16,
