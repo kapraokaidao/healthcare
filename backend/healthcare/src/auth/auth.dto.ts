08d297176bffc3fd9b3b8e6afe7f55ff1f79a6d2
Thanawat <taan02991@hotmail.com>
2021-01-27 12:34:09 +0700

    feat(auth): add patient log in api

--- backend/healthcare/src/auth/auth.dto.ts
+++ backend/healthcare/src/auth/auth.dto.ts
@@ -1,10 +1,23 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsNumberString, Length } from "class-validator";
 
 export class AuthCredentialsDto {
   @ApiProperty()
   username: string;
 
   @ApiProperty()
+  password: string;
+}
+
+export class PatientAuthCredentialsDto extends AuthCredentialsDto {
+  @ApiProperty()
+  @Length(13)
+  @IsNumberString()
+  username: string;
+
+  @ApiProperty()
+  @Length(6)
+  @IsNumberString()
   password: string;
 }
 
