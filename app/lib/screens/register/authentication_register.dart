e19ca3e7e9f9dc58b9970192d8360d7725ee1f10
Kao Oat <tanawit139@gmail.com>
2021-02-10 18:25:24 +0700

    fix auth

--- app/lib/screens/register/authentication_register.dart
+++ app/lib/screens/register/authentication_register.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:healthcare_app/authentication/bloc/authentication_bloc.dart';
 import 'package:healthcare_app/components/round_button.dart';
 import 'package:healthcare_app/components/styled_text_form_field.dart';
 import 'package:healthcare_app/repositories/index.dart';
@@ -12,6 +13,7 @@ class RegisterPage extends StatefulWidget {
   static route() {
     return MaterialPageRoute(builder: (_) => RegisterPage());
   }
+
   @override
   _RegisterPageState createState() => _RegisterPageState();
 }
@@ -35,142 +37,138 @@ class _RegisterPageState extends State<RegisterPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Container(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        padding: EdgeInsets.symmetric(vertical: 64, horizontal: 32),
-        decoration: BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage('assets/images/background.png'),
-              fit: BoxFit.cover
-            )
-        ),
-        child: SingleChildScrollView(
-          child: Form(
-            key: _formKey,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                FractionallySizedBox(
-                  child: Text("ชื่อ", style: TextStyle(fontSize: 16)),
-                  widthFactor: 1,
-                ),
-                Container(
-                  margin: EdgeInsets.only(top: 6, bottom: 16),
-                  child: StyledTextFormField(
-                    controller: firstnameController,
-                  ),
-                ),
-                FractionallySizedBox(
-                  child: Text("นามสกุล", style: TextStyle(fontSize: 16)),
-                  widthFactor: 1,
-                ),
-                Container(
-                  margin: EdgeInsets.only(top: 6, bottom: 16),
-                  child: StyledTextFormField(
-                    controller: lastnameController,
-                  ),
-                ),
-                FractionallySizedBox(
-                  child: Text("เลขประจำตัวประชาชน", style: TextStyle(fontSize: 16)),
-                  widthFactor: 1,
-                ),
-                Container(
-                  margin: EdgeInsets.only(top: 6, bottom: 16),
-                  child: StyledTextFormField(
-                    controller: nationalIdController,
-                  ),
-                ),
-                FractionallySizedBox(
-                  child: Text("รหัส pin", style: TextStyle(fontSize: 16)),
-                  widthFactor: 1,
-                ),
-                Container(
-                  margin: EdgeInsets.only(top: 6, bottom: 16),
-                  child: StyledTextFormField(
-                    controller: pinController,
-                  ),
-                ),
-                FractionallySizedBox(
-                  child: Text("เพศ", style: TextStyle(fontSize: 16)),
-                  widthFactor: 1,
-                ),
-                Container(
-                  margin: EdgeInsets.only(top: 6, bottom: 16),
-                  child: DropdownButton<String>(
-                    value: gender,
-                    onChanged: (String val) {
-                      setState(() {
-                        gender = val;
-                      });
-                    },
-                    items: [
-                      DropdownMenuItem<String>(
-                        value: "Male",
-                        child: Text("ชาย"),
+    return BlocBuilder<AuthenticationBloc, AuthenticationState>(builder: (ctx, state) {
+      return Scaffold(
+        backgroundColor: Colors.white,
+        body: Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            padding: EdgeInsets.symmetric(vertical: 64, horizontal: 32),
+            decoration: BoxDecoration(
+                image: DecorationImage(image: AssetImage('assets/images/background.png'), fit: BoxFit.cover)),
+            child: SingleChildScrollView(
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    FractionallySizedBox(
+                      child: Text("ชื่อ", style: TextStyle(fontSize: 16)),
+                      widthFactor: 1,
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(top: 6, bottom: 16),
+                      child: StyledTextFormField(
+                        controller: firstnameController,
                       ),
-                      DropdownMenuItem<String>(
-                        value: "Female",
-                        child: Text("หญิง"),
+                    ),
+                    FractionallySizedBox(
+                      child: Text("นามสกุล", style: TextStyle(fontSize: 16)),
+                      widthFactor: 1,
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(top: 6, bottom: 16),
+                      child: StyledTextFormField(
+                        controller: lastnameController,
                       ),
-                    ]
-                  ),
+                    ),
+                    FractionallySizedBox(
+                      child: Text("เลขประจำตัวประชาชน", style: TextStyle(fontSize: 16)),
+                      widthFactor: 1,
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(top: 6, bottom: 16),
+                      child: StyledTextFormField(
+                        controller: nationalIdController,
+                      ),
+                    ),
+                    FractionallySizedBox(
+                      child: Text("รหัส pin", style: TextStyle(fontSize: 16)),
+                      widthFactor: 1,
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(top: 6, bottom: 16),
+                      child: StyledTextFormField(
+                        controller: pinController,
+                      ),
+                    ),
+                    FractionallySizedBox(
+                      child: Text("เพศ", style: TextStyle(fontSize: 16)),
+                      widthFactor: 1,
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(top: 6, bottom: 16),
+                      child: DropdownButton<String>(
+                          value: gender,
+                          onChanged: (String val) {
+                            setState(() {
+                              gender = val;
+                            });
+                          },
+                          items: [
+                            DropdownMenuItem<String>(
+                              value: "Male",
+                              child: Text("ชาย"),
+                            ),
+                            DropdownMenuItem<String>(
+                              value: "Female",
+                              child: Text("หญิง"),
+                            ),
+                          ]),
+                    ),
+                    FractionallySizedBox(
+                      child: Text("เบอร์โทรศัพท์", style: TextStyle(fontSize: 16)),
+                      widthFactor: 1,
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(top: 6, bottom: 16),
+                      child: StyledTextFormField(
+                        controller: phoneController,
+                      ),
+                    ),
+                    FractionallySizedBox(
+                      child: Text("ที่อยู่", style: TextStyle(fontSize: 16)),
+                      widthFactor: 1,
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(top: 6, bottom: 16),
+                      child: StyledTextFormField(
+                        controller: addressController,
+                      ),
+                    ),
+                    FractionallySizedBox(
+                      child: Text("วันเกิด", style: TextStyle(fontSize: 16)),
+                      widthFactor: 1,
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(top: 6, bottom: 16),
+                      child: StyledTextFormField(
+                        controller: birthdateController,
+                      ),
+                    ),
+                    RoundButton(
+                      title: "submit",
+                      onPressed: () {
+                        Map<String, dynamic> user = _createRegisterBody(
+                          firstname: firstnameController.value.text,
+                          lastname: lastnameController.value.text,
+                          nationalId: nationalIdController.value.text,
+                          pin: pinController.value.text,
+                          phone: phoneController.value.text,
+                          address: addressController.value.text,
+                          birthdate: birthdateController.value.text,
+                          gender: gender,
+                        );
+                        HttpClient.post('/auth/register', user);
+                        print(jsonEncode(user));
+                      },
+                    )
+                  ],
                 ),
-                FractionallySizedBox(
-                  child: Text("เบอร์โทรศัพท์", style: TextStyle(fontSize: 16)),
-                  widthFactor: 1,
-                ),
-                Container(
-                  margin: EdgeInsets.only(top: 6, bottom: 16),
-                  child: StyledTextFormField(
-                    controller: phoneController,
-                  ),
-                ),
-                FractionallySizedBox(
-                  child: Text("ที่อยู่", style: TextStyle(fontSize: 16)),
-                  widthFactor: 1,
-                ),
-                Container(
-                  margin: EdgeInsets.only(top: 6, bottom: 16),
-                  child: StyledTextFormField(
-                    controller: addressController,
-                  ),
-                ),
-                FractionallySizedBox(
-                  child: Text("วันเกิด", style: TextStyle(fontSize: 16)),
-                  widthFactor: 1,
-                ),
-                Container(
-                  margin: EdgeInsets.only(top: 6, bottom: 16),
-                  child: StyledTextFormField(
-                    controller: birthdateController,
-                  ),
-                ),
-                RoundButton(
-                  title: "submit",
-                  onPressed: () {
-                    Map<String, dynamic> user = _createRegisterBody(
-                      firstname: firstnameController.value.text,
-                      lastname: lastnameController.value.text,
-                      nationalId: nationalIdController.value.text,
-                      pin: pinController.value.text,
-                      phone: phoneController.value.text,
-                      address: addressController.value.text,
-                      birthdate: birthdateController.value.text,
-                      gender: gender,
-                    );
-                    HttpClient.post('/auth/register', user);
-                    print(jsonEncode(user));
-                  },
-                )
-              ],
-            ),
-          ),
-        )
-      ),
-    );
+              ),
+            )),
+      );
+    });
   }
 
   Map<String, dynamic> _createRegisterBody({
@@ -201,7 +199,7 @@ class _RegisterPageState extends State<RegisterPage> {
 
     return user;
   }
-  /*
+/*
   *   final _formKey = GlobalKey<FormState>();
   final firstnameController = TextEditingController();
   final lastnameController = TextEditingController();
