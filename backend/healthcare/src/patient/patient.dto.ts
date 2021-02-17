5bcf5f14cebadd81750109f75946ab46623e33ff
Kao Oat <tanawit139@gmail.com>
2021-02-18 02:13:50 +0700

    feat(patient): add register patient v2

--- backend/healthcare/src/patient/patient.dto.ts
+++ backend/healthcare/src/patient/patient.dto.ts
@@ -1,6 +1,52 @@
-import { IsEnum, IsNumber, IsString, Max, Min } from "class-validator";
+import {
+  IsDate,
+  IsEnum,
+  IsNotEmpty,
+  IsNumber,
+  IsNumberString,
+  IsString,
+  Length,
+  Matches,
+  Max,
+  Min,
+} from "class-validator";
 import { BloodRh, BloodType } from "../constant/enum/patient.enum";
 import { ApiProperty } from "@nestjs/swagger";
+import { UserGender } from "../constant/enum/user.enum";
+
+export class PatientRegisterDto {
+  @ApiProperty()
+  @IsNumberString()
+  @Length(13, 13)
+  nationalId: string;
+
+  @ApiProperty()
+  @IsNumberString()
+  @Length(6, 6)
+  pin: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  firstname: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  lastname: string;
+
+  @ApiProperty()
+  @Matches(/0[0-9]{9}/)
+  phone: string;
+
+  @ApiProperty()
+  address: string;
+
+  @ApiProperty({ type: "enum", enum: UserGender })
+  @IsEnum(UserGender)
+  gender: UserGender;
+
+  @ApiProperty()
+  birthDate: Date;
+}
 
 export class PatientInfoUpdateDto {
   @ApiProperty()
