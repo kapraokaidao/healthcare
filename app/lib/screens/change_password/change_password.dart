d422c4a4bbdeb17a5392c60f49589d4f3964e4e7
Thanadol <thanafr1998@hotmail.com>
2021-02-24 17:25:39 +0700

    change password page

---
+++ app/lib/screens/change_password/change_password.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:healthcare_app/components/my_bottom_nav_bar.dart';
+import 'package:healthcare_app/constants.dart';
+import 'package:healthcare_app/screens/change_password/components/body.dart';
+import 'package:healthcare_app/size_config.dart';
+
+class ChangePassword extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    SizeConfig().init(context);
+    return Scaffold(
+      appBar: buildAppBar(),
+      body: Body(),
+      bottomNavigationBar: MyBottomNavBar(),
+    );
+  }
+
+  AppBar buildAppBar() {
+    return AppBar(
+      backgroundColor: kPrimaryColor,
+      leading: SizedBox(),
+      // On Android it's false by default
+      centerTitle: true,
+    );
+  }
+}
