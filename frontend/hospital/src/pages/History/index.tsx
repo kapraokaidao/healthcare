ac5afe8e7e11e21aedd507477243f78f611b9140
N <n@n.n>
2021-02-24 17:14:30 +0700

    fix(frontend hospital): history, summary

--- frontend/hospital/src/pages/History/index.tsx
+++ frontend/hospital/src/pages/History/index.tsx
@@ -1,68 +1,78 @@
-import AppBar from '@material-ui/core/AppBar';
-import Box from '@material-ui/core/Box';
-import Tab from '@material-ui/core/Tab';
-import Tabs from '@material-ui/core/Tabs';
-import Typography from '@material-ui/core/Typography';
-import { observer } from 'mobx-react-lite';
-import React, { useContext, useEffect, useState } from 'react';
-import { TitleContext } from '../../App';
-import Credit from './Credit';
-import Debit from './Debit';
+import AppBar from "@material-ui/core/AppBar";
+import Box from "@material-ui/core/Box";
+import Tab from "@material-ui/core/Tab";
+import Tabs from "@material-ui/core/Tabs";
+import Typography from "@material-ui/core/Typography";
+import { observer } from "mobx-react-lite";
+import React, { useContext, useEffect, useState } from "react";
+import { TitleContext } from "../../App";
+import Provide from "./Provide";
+import Redeem from "./Redeem";
+import Withdraw from "./Withdraw";
+import { AuthStoreContext } from "../../stores";
 
 function TabPanel(props: { [x: string]: any; children: any; value: any; index: any }) {
-	const { children, value, index, ...other } = props;
-	return (
-		<div
-			role="tabpanel"
-			hidden={value !== index}
-			id={`simple-tabpanel-${index}`}
-			aria-labelledby={`simple-tab-${index}`}
-			{...other}
-		>
-			{value === index && (
-				<Box p={3}>
-					<Typography>{children}</Typography>
-				</Box>
-			)}
-		</div>
-	);
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={3}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
 }
 
 function a11yProps(index: number) {
-	return {
-		id: `full-width-tab-${index}`,
-		'aria-controls': `full-width-tabpanel-${index}`,
-	};
+  return {
+    id: `full-width-tab-${index}`,
+    "aria-controls": `full-width-tabpanel-${index}`,
+  };
 }
 
 const History = observer(() => {
-	const { setTitle } = useContext(TitleContext);
-	const [value, setValue] = useState(0);
-	useEffect(() => {
-		setTitle('Summary');
-	}, [setTitle]);
+  const authStore = useContext(AuthStoreContext);
+  const { setTitle } = useContext(TitleContext);
+  const [value, setValue] = useState(0);
+  useEffect(() => {
+    setTitle("Summary");
+  }, [setTitle]);
 
-	const handleChange = (event: any, newValue: React.SetStateAction<number>) => {
-		setValue(newValue);
-	};
+  const handleChange = (event: any, newValue: React.SetStateAction<number>) => {
+    setValue(newValue);
+  };
 
-	return (
-		<>
-			<h1>History</h1>
-			<AppBar position="static">
-				<Tabs value={value} onChange={handleChange} aria-label="simple tabs example" centered>
-					<Tab label="Receiving history" {...a11yProps(0)} />
-					<Tab label="Giving history" {...a11yProps(1)} />
-				</Tabs>
-			</AppBar>
-			<TabPanel value={value} index={0}>
-				<Debit />
-			</TabPanel>
-			<TabPanel value={value} index={1}>
-				<Credit />
-			</TabPanel>
-		</>
-	);
+  return (
+    <>
+      <h1>History</h1>
+      <AppBar position="static">
+        <Tabs value={value} onChange={handleChange} aria-label="simple tabs example" centered>
+          <Tab label="Redeem history" {...a11yProps(0)} />
+          <Tab label="Provide history" {...a11yProps(1)} />
+          {authStore.user?.role === "HospitalAdmin" && <Tab label="Withdraw history" {...a11yProps(3)} />}
+        </Tabs>
+      </AppBar>
+      <TabPanel value={value} index={0}>
+        <Redeem />
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        <Provide />
+      </TabPanel>
+      {authStore.user?.role === "HospitalAdmin" && (
+        <TabPanel value={value} index={2}>
+          <Withdraw />
+        </TabPanel>
+      )}
+    </>
+  );
 });
 
 export default History;
---
+++ frontend/hospital/src/pages/Summary/Provide/index.tsx
@@ -0,0 +1,125 @@
+import Checkbox from "@material-ui/core/Checkbox";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import TextField from "@material-ui/core/TextField";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { TokenDetail, User } from "../../../types";
+
+type ProvideHistory = {
+  name: String;
+  amount: number;
+};
+
+const Provide = () => {
+  const [histories, setHistories] = useState<ProvideHistory[]>([]);
+  const [selectedStartDate, setSelectedStartDate] = useState({
+    checked: false,
+    date: new Date(),
+  });
+  const [selectedEndDate, setSelectedEndDate] = useState({
+    checked: false,
+    date: new Date(),
+  });
+
+  useEffect(() => {
+    const filter: any = {};
+    if (selectedStartDate.checked) filter["startDate"] = selectedStartDate.date.toISOString().split("T")[0];
+    if (selectedEndDate.checked) filter["endDate"] = selectedEndDate.date.toISOString().split("T")[0];
+    axios
+      .post("/transaction/search/group-by-service", {
+        type: "Provide",
+        ...filter,
+      })
+      .then(({ data }) => {
+        setHistories(data);
+      });
+  }, [selectedStartDate, selectedEndDate]);
+
+  return (
+    <>
+      <table>
+        <tr>
+          <td>Start Date</td>
+          <td>
+            <Checkbox
+              checked={selectedStartDate.checked}
+              onChange={(e) => {
+                setSelectedStartDate({
+                  date: selectedStartDate.date,
+                  checked: e.target.checked,
+                });
+              }}
+              color="primary"
+            />
+          </td>
+          <td>
+            <TextField
+              type="date"
+              defaultValue={selectedStartDate.date.toISOString().split("T")[0]}
+              onChange={(e) => {
+                setSelectedStartDate({
+                  date: new Date(e.target.value),
+                  checked: selectedStartDate.checked,
+                });
+              }}
+            />
+          </td>
+        </tr>
+        <tr>
+          <td>End Date</td>
+          <td>
+            <Checkbox
+              checked={selectedEndDate.checked}
+              onChange={(e) => {
+                setSelectedEndDate({
+                  date: selectedEndDate.date,
+                  checked: e.target.checked,
+                });
+              }}
+              name="checkedB"
+              color="primary"
+            />
+          </td>
+          <td>
+            <TextField
+              type="date"
+              defaultValue={selectedEndDate.date.toISOString().split("T")[0]}
+              onChange={(e) => {
+                setSelectedEndDate({
+                  date: new Date(e.target.value),
+                  checked: selectedEndDate.checked,
+                });
+              }}
+            />
+          </td>
+        </tr>
+      </table>
+      <div style={{ height: 700, width: "100%" }}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Healthcare</TableCell>
+              <TableCell>Amount</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {histories.map((history) => {
+              return (
+                <TableRow>
+                  <TableCell>{history.name}</TableCell>
+                  <TableCell>{history.amount}</TableCell>
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </div>
+    </>
+  );
+};
+
+export default Provide;
