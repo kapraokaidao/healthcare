a07c139019f3392391ab064bbcb6044c39f00636
Thanawat <taan02991@hotmail.com>
2021-03-24 14:10:28 +0700

    feat(transaction): search transaction for NHSO

--- backend/healthcare/src/transaction/transaction.dto.ts
+++ backend/healthcare/src/transaction/transaction.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsEnum } from "class-validator";
+import { IsEnum, IsNumberString, Length } from "class-validator";
 import { TxType } from "src/constant/enum/transaction.enum";
 
 export class TransactionSearchDto {
@@ -42,4 +42,49 @@ export class TransactionSearchResponseDto {
   id: number;
   name: string;
   amount: number;
+}
+
+export class SearchTransactionNHSODto {
+  @ApiProperty({required: false})
+  @Length(9, 9)
+  @IsNumberString()
+  hospitalCode: string;
+
+  @ApiProperty({ required: false })
+  startDate: Date;
+
+  @ApiProperty({ required: false })
+  endDate: Date;
+
+  @ApiProperty({ required: false })
+  serviceName: string;
+
+  @ApiProperty({ required: false })
+  firstname: string;
+
+  @ApiProperty({ required: false })
+  lastname: string;
+
+  @ApiProperty()
+  page: number;
+
+  @ApiProperty()
+  pageSize: number;
+
+}
+
+export class SearchTransactionNHSOResponse {
+  id: number
+
+  firstname: string
+
+  lastname: string
+
+  serviceName: string
+
+  amount: number
+
+  createdDate: Date
+
+  hospitalName: string
 }
